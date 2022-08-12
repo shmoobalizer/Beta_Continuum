e3676f26271c7f47514c7f93ba9581bcd755fecd
shmoobalizer <53633630+shmoobalizer@users.noreply.github.com>
2022-08-12 02:57:31 -0400

    Add files via upload

--- assets/minecraft/shaders/include/flat_item.glsl
+++ assets/minecraft/shaders/include/flat_item.glsl
@@ -1,10 +1,10 @@
+
 const vec2[4] corners = vec2[4](vec2(-1, 1), vec2(-1, -1), vec2(1, -1), vec2(1, 1));
 vec2 corner = corners[gl_VertexID % 4];
 
 vec2 texSize = textureSize(Sampler0, 0);
 
 gl_Position = ProjMat * ModelViewMat * vec4(Position, 1.0);
-vertexColor = minecraft_mix_light(Light0_Direction, Light1_Direction, Normal, Color) * texelFetch(Sampler2, UV2 / 16, 0);
 
 if (FogStart < 100000 && texSize.x > 256 && abs(length(Normal) - 0.625) < 0.01) //Not in hand, atlas is not a mob, weird Normal length from display.
 {
@@ -24,6 +24,4 @@ if (FogStart < 100000 && texSize.x > 256 && abs(length(Normal) - 0.625) < 0.01) 
 
         gl_Position = ProjMat * ModelViewMat * vec4(Position + offset * 0.25, 1.0);
     }
-
-    vertexColor = texelFetch(Sampler2, UV2 / 16, 0) * Color;
 }
